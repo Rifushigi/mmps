cdecf825771be304260b0709f5d88de0ff7227b2
rifushigi <tope.dansa@gmail.com>
2025-03-08 19:42:00 +0100

    Refactor app setup

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,12 +1,33 @@
 //Under development
 
-import { PORT, app, createDB } from "./config";
+import { PORT, createDB, corsOptions } from "./config";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import express, { json, urlencoded, Application, Request, Response } from "express";
+import { notFound, errHandler } from "./middlewares";
+import allRoutes from "./routes";
+import path from 'path';
+import { monkeyPatchFaceAPI } from './utils/faceapi.env';
+// import '@tensorflow/tfjs-node';
 
-const startApp = async (port: string) => {
-    await createDB();
-    return app.listen(port, () => {
-        console.log(`server is listening on port ${port}`);
-    });
-};
+monkeyPatchFaceAPI();
+createDB();
 
-startApp(PORT);
+const app: Application = express();
+
+app.use(cors(corsOptions));
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+// Serve face-api.js models
+app.use('/models', express.static(path.join(__dirname, '../public/models')))
+app.use("/v1", allRoutes);
+app.use(notFound);
+app.use(errHandler);
+
+app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`);
+});
