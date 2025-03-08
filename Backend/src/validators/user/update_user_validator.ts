1c1d8a4aab08502011650c4c34ce2d789911230b
rifushigi <tope.dansa@gmail.com>
2025-03-08 19:31:49 +0100

    Renamed files

--- Backend/src/validators/user/update_user_validator.ts
+++
@@ -1,7 +0,0 @@
-import Joi from "joi";
-
-// UPDATE USER SCHEMA (allow partial updates)
-export const updateUserSchema = Joi.object({
-    name: Joi.string().optional(),
-    email: Joi.string().email().optional(),
-}).min(1); // Require at least one field to be updated
