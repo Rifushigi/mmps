ed92b52e5ff2fb2103bd5be76270d2852c5a9d99
rifushigi <tope.dansa@gmail.com>
2025-03-08 20:20:22 +0100

    Refactor user routes to use user controller and improve comments

--- Backend/src/routes/v1/api/user.routes.ts
+++ Backend/src/routes/v1/api/user.routes.ts
@@ -1,38 +1,40 @@
-//Under development
+//TODO
 import { Router } from "express";
-import { UserController } from "../../../controllers";
+import user from "../../../controllers/user.controller";
 import { updateUserSchema } from "../../../validators";
 import { validator, authenticateToken, isAdmin } from "../../../middlewares";
 
-const { deleteUser, getAllUsers, getUserById, updateUser, getTotalUsers } = new UserController();
-
 const userRouter: Router = Router();
 
-userRouter.get("/total", getTotalUsers)
+// Get total number of admin users
+userRouter.get(
+    "/total",
+    user.getTotalUsers
+)
 
-// READ - Get all users (Admin only)
+// Get all users (Admin only)
 userRouter.get(
     "/all",
-    getAllUsers
+    user.getAllUsers
 );
 
-// READ - Get a specific user by ID (Admin only)
+// Get a specific user by ID (Admin only)
 userRouter.get(
     "/:id",
-    getUserById
+    user.getUserById
 );
 
-// UPDATE - Update a user by ID (Admin or self)
+// Update a user by ID (Admin or self)
 userRouter.patch(
     "/:id",
     validator(updateUserSchema),
-    updateUser
+    user.updateUser
 );
 
-// DELETE - Delete a user by ID (Admin only)
+// Delete a user by ID (Admin only)
 userRouter.delete(
     "/:id",
-    deleteUser
+    user.deleteUser
 );
 
 export default userRouter;
