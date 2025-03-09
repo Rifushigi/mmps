6f42a0b3ecd9faccb56cd5e73d43a473429689e4
rifushigi <tope.dansa@gmail.com>
2025-03-09 17:41:00 +0100

    Enhance UserDetailView UI: update background and text colors, improve card styles, and fix data fetching logic for better visibility

--- Frontend/mmps/src/views/UserDetailView.vue
+++ Frontend/mmps/src/views/UserDetailView.vue
@@ -1,11 +1,11 @@
 <!-- src/views/UserDetailView.vue -->
 <template>
   <div
-    class="bg-slate-800 flex flex-col justify-start items-center pb-10 h-dvh overflow-y-auto w-screen"
+    class="bg-gray-100 flex flex-col justify-start items-center pb-10 h-dvh overflow-y-auto w-screen"
   >
     <AdminHeader />
     <div class="flex flex-col items-center h-full w-full">
-      <h1 class="text-3xl font-bold text-gray-300 mb-8 mt-4">User Details</h1>
+      <h1 class="text-3xl font-bold text-gray-800 mb-8 mt-4">User Details</h1>
 
       <button
         @click="goBack"
@@ -16,8 +16,8 @@
 
       <div class="flex w-full mt-8 px-32 max-h-500px">
         <!-- Left Menu or Navigation -->
-        <div class="min-w-[200px] w-[500px] lg:w-1/3 mr-4 h-60 bg-gray-800 rounded-l-lg p-4">
-          <h2 class="text-2xl text-white my-2 mx-2 font-bold mb-4">User Details</h2>
+        <div class="min-w-[200px] w-[500px] lg:w-1/3 mr-4 h-60 bg-white rounded-l-lg p-4 shadow-md">
+          <h2 class="text-2xl text-gray-800 my-2 mx-2 font-bold mb-4">User Details</h2>
           <ul>
             <li
               @click="currentTab = 'details'"
@@ -47,71 +47,61 @@
         <div
           class="min-w-fit w-[600px] max-h-[60dvh] lg:w-1/2 p-4 bg-white shadow-md rounded-r-lg overflow-y-auto"
         >
-          <div v-if="currentTab === 'details'" class="text-white p-5 h-full w-full">
-            <h2 class="text-2xl text-gray-800 font-bold mb-3">{{ user.name }}</h2>
+          <div v-if="currentTab === 'details'" class="text-gray-800 p-5 h-full w-full">
+            <h2 class="text-2xl font-bold mb-3">{{ user.name }}</h2>
             <h3 class="text-xl mb-4 text-gray-600">Scores of Previous Quizzes</h3>
             <div class="overflow-x-auto">
               <table
-                class="w-full mb-4 bg-gray-800 rounded-lg overflow-y-auto table-auto border-2 border-gray-500"
+                class="w-full mb-4 bg-gray-100 rounded-lg overflow-y-auto table-auto border-2 border-gray-300"
               >
-                <thead class="bg-gray-300 border-b-2 border-gray-500">
+                <thead class="bg-gray-200 border-b-2 border-gray-300">
                   <tr>
                     <th
-                      class="px-4 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider border-2 border-gray-500"
+                      class="px-4 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider border-2 border-gray-300"
                     >
                       Quiz Name
                     </th>
                     <th
-                      class="px-4 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider border-2 border-gray-500"
+                      class="px-4 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider border-2 border-gray-300"
                     >
                       Score
                     </th>
                     <th
-                      class="px-4 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider border-2 border-gray-500"
+                      class="px-4 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider border-2 border-gray-300"
                     >
                       Obtainable Score
                     </th>
                     <th
-                      class="px-4 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider border-2 border-gray-500"
+                      class="px-4 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider border-2 border-gray-300"
                     >
                       Date
                     </th>
                     <th
-                      class="px-4 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider border-2 border-gray-500"
+                      class="px-4 py-3 text-left text-xs font-medium text-gray-800 uppercase tracking-wider border-2 border-gray-300"
                     >
                       Time
                     </th>
                   </tr>
                 </thead>
-                <tbody class="divide-y-2 divide-gray-800">
+                <tbody class="divide-y-2 divide-gray-200">
                   <tr
                     v-for="quiz in user.quizzes"
                     :key="quiz.date"
-                    class="hover:bg-gray-400 bg-gray-300 border-2 border-gray-500"
+                    class="hover:bg-gray-300 bg-gray-200 border-2 border-gray-300"
                   >
-                    <td
-                      class="px-4 py-3 whitespace-nowrap text-gray-800 border-2 border-gray-500"
-                    >
+                    <td class="px-4 py-3 whitespace-nowrap text-gray-800 border-2 border-gray-300">
                       {{ quiz.quizName }}
                     </td>
-                    <td
-                      class="px-4 py-3 whitespace-nowrap text-gray-800 border-2 border-gray-500"
-                    >
+                    <td class="px-4 py-3 whitespace-nowrap text-gray-800 border-2 border-gray-300">
                       {{ quiz.correctAnswers }}
                     </td>
-                    <td
-                      class="px-4 py-3 whitespace-nowrap text-gray-800 border-2 border-gray-500"
-                    >
+                    <td class="px-4 py-3 whitespace-nowrap text-gray-800 border-2 border-gray-300">
                       {{ quiz.totalQuestions }}
                     </td>
-                    <td
-                      class="px-4 py-3 whitespace-nowrap text-gray-800 border-2 border-gray-500"
-                    >
+                    <td class="px-4 py-3 whitespace-nowrap text-gray-800 border-2 border-gray-300">
                       {{ formatDate(quiz.date) }}
                     </td>
-                    <td
-                      class="px-4 py-3 whitespace-nowrap text-gray-800 border-2 border-gray-500"
-                    >
+                    <td class="px-4 py-3 whitespace-nowrap text-gray-800 border-2 border-gray-300">
                       {{ formatTime(quiz.date) }}
                     </td>
                   </tr>
@@ -191,8 +181,8 @@ const goBack = () => {
 // Fetch user data based on userId
 const fetchUserData = async (id) => {
   try {
-    const response = await axiosInstance.get(`/users/${id}`)
-    const { _id, ...rest } = response.data
+    const response = await axiosInstance.get(`/user/${id}`)
+    const { _id, ...rest } = response.data.data
     user.value = { id: _id, ...rest }
     console.log(user.value.id)
   } catch (error) {
@@ -202,7 +192,7 @@ const fetchUserData = async (id) => {
 
 const fetchUserQuizData = async (id) => {
   const response = await axiosInstance.get(`/quiz/user/${id}`)
-  user.value.quizzes = response.data
+  user.value.quizzes = response.data.data
 }
 
 onMounted(() => {
@@ -214,7 +204,7 @@ onMounted(() => {
 const updateUser = async () => {
   try {
     console.log(user.value)
-    await axiosInstance.patch(`/users/${user.value.id}`, {
+    await axiosInstance.patch(`/user/${user.value.id}`, {
       ...user.value,
       id: undefined,
       _id: undefined,
@@ -233,7 +223,7 @@ const updateUser = async () => {
 const deleteUser = async () => {
   try {
     console.log(user.value.id)
-    await axiosInstance.delete(`/users/${user.value.id}`)
+    await axiosInstance.delete(`/user/${user.value.id}`)
     console.log('User deleted successfully')
     alert('User deleted successfully')
   } catch (error) {
@@ -261,7 +251,7 @@ const formatTime = (dateString) => {
 <style scoped>
 .nav-link {
   position: relative;
-  @apply text-gray-300 text-lg hover:text-white px-4 py-2 rounded-full transition-all duration-200;
+  @apply text-gray-800 text-lg hover:text-blue-500 px-4 py-2 rounded-full transition-all duration-200;
 }
 
 .nav-link::after {
@@ -271,12 +261,64 @@ const formatTime = (dateString) => {
   bottom: -1px;
   width: 0px;
   height: 4px;
-  background-color: white;
+  background-color: #3182ce;
   transition: width 0.3s ease-in-out;
+}
+
+.nav-link:hover::after {
+  width: 100%;
 }
 
 .active-link::after {
   width: 65%;
-  background-color: white;
+  background-color: #3182ce;
+}
+
+.bg-gray-100 {
+  background-color: #f7fafc;
+}
+
+.text-gray-800 {
+  color: #2d3748;
+}
+
+.text-gray-600 {
+  color: #718096;
+}
+
+.bg-white {
+  background-color: #ffffff;
+}
+
+.shadow-md {
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.rounded-l-lg {
+  border-radius: 0.5rem 0 0 0.5rem;
+}
+
+.rounded-r-lg {
+  border-radius: 0 0.5rem 0.5rem 0;
+}
+
+.bg-blue-600 {
+  background-color: #3182ce;
+}
+
+.bg-blue-700 {
+  background-color: #2b6cb0;
+}
+
+.text-white {
+  color: #ffffff;
+}
+
+.bg-red-500 {
+  background-color: #e53e3e;
+}
+
+.bg-red-700 {
+  background-color: #c53030;
 }
 </style>
