4c545d481c291774d26beb5555b0abf1f485759b
rifushigi <tope.dansa@gmail.com>
2025-03-09 02:30:03 +0100

    Refactor quiz routes: streamline route definitions and enhance readability

--- Backend/src/routes/v1/api/quiz.routes.ts
+++ Backend/src/routes/v1/api/quiz.routes.ts
@@ -9,18 +9,25 @@ const quizRouter: Router = Router();
 
 // Get the total number of quizzes
 quizRouter.get("/total", quiz.getTotalQuizzes)
+
 // Create a new quiz (Admin only)
 quizRouter.post("/", validate(createQuizSchema), quiz.createQuiz);
+
 // Get a specific quiz by ID
 quizRouter.get("/:id", quiz.getQuizById);
+
 // Get all quizzes (potentially paginated)
 quizRouter.get("/", quiz.getAllQuizzes);
+
 // Update a quiz by ID (Admin only)
 quizRouter.patch("/:id", validate(updateQuizSchema), quiz.updateQuiz);
+
 // Delete a quiz by ID (Admin only)
 quizRouter.delete("/:id", quiz.deleteQuiz);
+
 // Get the results of quiz by ID
 quizRouter.get('/user/:userId', quizResult.getQuizResultsByUserId);
+
 // save the result of the quiz
 quizRouter.post('/quiz-result', quizResult.saveQuizResult);
 
