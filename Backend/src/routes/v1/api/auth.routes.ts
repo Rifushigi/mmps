53dee0862ce23ebbc686fc790ecf1978dff96d17
rifushigi <tope.dansa@gmail.com>
2025-03-09 01:56:44 +0100

    Refactor auth routes: update import statements and streamline route definitions

--- Backend/src/routes/v1/api/auth.routes.ts
+++ Backend/src/routes/v1/api/auth.routes.ts
@@ -1,37 +1,23 @@
 //Under development
-import { validator } from "../../../middlewares";
-import { AuthController } from "../../../controllers";
+import { validate } from "../../../middlewares";
+import auth from "../../../controllers/auth.controller";
 import { Router } from "express";
-import {
-    signupSchema,
-    loginSchema,
-} from "../../../validators";
-
-const {
-    login,
-    refresh,
-    logout,
-    signup,
-    validateFace
-} = new AuthController();
-
+import { signupSchema, loginSchema } from "../../../validators";
 // import {authenticateToken} from "../middlewares";
 
 const authRouter: Router = Router();
-
+//TODO
 // admin or user logs in
-authRouter.route("/login").post(validator(loginSchema), login);
-
-authRouter.route("/signup").post(validator(signupSchema), signup)
-
+authRouter.post("/login", validate(loginSchema), auth.login);
+// user signs up
+authRouter.post("/signup", validate(signupSchema), auth.signup)
 // checks if the refresh token stored in cookie is valid and renews the accesstoken
-authRouter.route("/refresh").get(refresh);
-
+authRouter.get("/refresh", auth.refresh);
 // // reset password after confirming otp
 // authRouter.route("/reset").patch(validator(resetPassSchema), resetPassword);
-
 // clears cookies and loggs out user
-authRouter.route("/logout").get(logout);
-authRouter.route("/validate-face").post(validateFace);
+authRouter.get("/logout", auth.logout);
+// validatees user using biometrics(face features)
+authRouter.post("/validate-face", auth.validateFace);
 
 export default authRouter;
