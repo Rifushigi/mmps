ab162a94bc9f866da3314adca76016d79b586970
rifushigi <tope.dansa@gmail.com>
2025-03-09 01:50:14 +0100

    Replace error handler middleware with global error handler

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,32 +1,29 @@
-//Under development
-
 import { PORT, createDB, corsOptions } from "./config";
 import cors from "cors";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import express, { json, urlencoded, Application, Request, Response } from "express";
-import { notFound, errHandler } from "./middlewares";
+import { notFound, globalErrorHandler } from "./middlewares";
 import allRoutes from "./routes";
 import path from 'path';
 import { monkeyPatchFaceAPI } from './utils/faceapi.env';
-// import '@tensorflow/tfjs-node';
 
 monkeyPatchFaceAPI();
 createDB();
 
 const app: Application = express();
 
+//middlewares
 app.use(cors(corsOptions));
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(morgan("dev"));
-
 // Serve face-api.js models
 app.use('/models', express.static(path.join(__dirname, '../public/models')))
 app.use("/v1", allRoutes);
 app.use(notFound);
-app.use(errHandler);
+app.use(globalErrorHandler);
 
 app.listen(PORT, () => {
     console.log(`server is listening on port ${PORT}`);
