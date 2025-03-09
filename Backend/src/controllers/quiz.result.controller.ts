22c86d945dc57ba94a87d91254c265a16fa57943
rifushigi <tope.dansa@gmail.com>
2025-03-09 02:24:58 +0100

    Refactor QuizResultController: streamline methods, implement async error handling, and enhance response structure

--- Backend/src/controllers/quiz.result.controller.ts
+++ Backend/src/controllers/quiz.result.controller.ts
@@ -1,64 +1,45 @@
-// controllers/QuizResultController.ts
 import { Request, Response, NextFunction } from 'express';
-import {QuizResult} from '../models';
-import { Quiz } from '../models';
+import { QuizResult, Quiz } from '../models';
+import { asyncErrorHandler } from '../middlewares';
+import { ValidationError, NotFoundError } from '../middlewares/err.handler.middleware';
+import { ApiResponse } from '../interfaces';
 
-export class QuizResultController {
+class QuizResultController {
 
-    async saveQuizResult(req: Request, res: Response, next: NextFunction) {
-        try {
-            const {
-                userId,
-                quizId,
-                quizName,
-                timeTaken,
-                totalQuestions,
-                answeredQuestions,
-                correctAnswers
-            } = req.body;
-
-            const newQuizResult = new QuizResult({
-                userId,
-                quizId,
-                date: new Date(),
-                quizName,
-                timeTaken,
-                totalQuestions,
-                answeredQuestions,
-                correctAnswers
-            });
-
-            const quiz = await Quiz.findById(quizId);
-            if (!quiz) {
-                return res.status(404).json({ message: "Quiz not found" });
-            }
-
-            // Add the user's ID to the taken array if not already present
-            if (!quiz.taken.includes(userId)) {
-                quiz.taken.push(userId);
-                await quiz.save();
-            }
-            const savedQuizResult = await newQuizResult.save();
-
-            res.status(201).json({ message: 'Quiz result saved successfully', quizResult: savedQuizResult });
-        } catch (error) {
-            next(error);
+    static saveQuizResult = asyncErrorHandler(async (req: Request, res: Response, next: NextFunction) => {
+        const { userId, quizId, quizName, timeTaken, totalQuestions, answeredQuestions, correctAnswers } = req.body;
+        const newQuizResult = new QuizResult({ userId, quizId, date: new Date(), quizName, timeTaken, totalQuestions, answeredQuestions, correctAnswers });
+        const quiz = await Quiz.findById(quizId);
+        if (!quiz) {
+            throw new NotFoundError("Quiz not found");
         }
-    }
-
-    async getQuizResultsByUserId(req: Request, res: Response, next: NextFunction) {
-        try {
-            const userId = req.params.userId;
-            const quizResults = await QuizResult.find({ userId });
-
-            if (!quizResults || quizResults.length === 0) {
-                res.status(404).json({ message: 'No quiz results found for this user' });
-                return;
-            }
-
-            res.status(200).json(quizResults);
-        } catch (error) {
-            next(error);
+        // Add the user's ID to the taken array if not already present
+        if (!quiz.taken.includes(userId)) {
+            quiz.taken.push(userId);
+            await quiz.save();
         }
-    }
+        const savedQuizResult = await newQuizResult.save();
+        const response: ApiResponse = {
+            status: true,
+            message: 'Quiz result saved successfully',
+            data: savedQuizResult
+        };
+        return res.status(201).json(response);
+    });
+
+    static getQuizResultsByUserId = asyncErrorHandler(async (req: Request, res: Response, next: NextFunction) => {
+        const userId = req.params.userId;
+        const quizResults = await QuizResult.find({ userId });
+        if (!quizResults || quizResults.length === 0) {
+            throw new NotFoundError('No quiz results found for this user');
+        }
+        const response: ApiResponse = {
+            status: true,
+            message: 'Quiz results retrieved successfully',
+            data: quizResults
+        };
+        return res.json(response);
+    });
 }
+
+export default QuizResultController;
