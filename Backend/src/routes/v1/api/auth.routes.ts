aeebd40e84c3f90a7db0118af0efb9c3646109fc
rifushigi <tope.dansa@gmail.com>
2025-03-09 02:29:56 +0100

    Refactor auth routes: streamline route definitions and enhance readability

--- Backend/src/routes/v1/api/auth.routes.ts
+++ Backend/src/routes/v1/api/auth.routes.ts
@@ -9,14 +9,18 @@ const authRouter: Router = Router();
 //TODO
 // admin or user logs in
 authRouter.post("/login", validate(loginSchema), auth.login);
+
 // user signs up
 authRouter.post("/signup", validate(signupSchema), auth.signup)
+
 // checks if the refresh token stored in cookie is valid and renews the accesstoken
 authRouter.get("/refresh", auth.refresh);
+
 // // reset password after confirming otp
 // authRouter.route("/reset").patch(validator(resetPassSchema), resetPassword);
 // clears cookies and loggs out user
 authRouter.get("/logout", auth.logout);
+
 // validatees user using biometrics(face features)
 authRouter.post("/validate-face", auth.validateFace);
 
