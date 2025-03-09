403801af340ba44f53ec5633a30aa344bd7e229c
rifushigi <tope.dansa@gmail.com>
2025-03-09 02:46:01 +0100

    Refactor constants configuration: rename NODE_ENV to ENV for consistency

--- Backend/src/config/constants.config.ts
+++ Backend/src/config/constants.config.ts
@@ -7,7 +7,7 @@ const PORT: string = process.env.PORT || "3000";
 const passphrase = process.env.PASSPHRASE;
 const localUrl = process.env.MONGO_LOCAL_URL;
 const webUrl = process.env.MONGO_PRODUCTION_URL;
-const nodeEnv = process.env.NODE_ENV;
+const env = process.env.ENV;
 const privateKey = process.env.PRIVATE_KEY;
 const publicKey = process.env.PUBLIC_KEY;
 
@@ -16,7 +16,7 @@ export {
     passphrase,
     localUrl,
     webUrl,
-    nodeEnv,
+    env,
     privateKey,
     publicKey,
 };
